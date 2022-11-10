16e6826a82f8fff8f6c197c91d323f2cde76924d
Young <kimyou2019@gmail.com>
2022-11-10 07:52:53 +0100

    add not found and add useTitle hook

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import useTitle from '../hooks/useTitle';
 
 function NotFound() {
+  useTitle('Not Found');
   return <div>NotFound</div>;
 }
 
--- src/pages/Public.jsx
+++ src/pages/Public.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import useTitle from '../hooks/useTitle';
 
 function Public() {
+  useTitle('Yoo Test');
   return (
     <div>Public</div>
   )
